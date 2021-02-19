6675bd1dcc058d18b9e2783facf0aa6ea9716d0c
xiao.xue <xiao.xue@RVSG4161>
2021-02-19 11:18:23 +0800

    Code to output data into the 3 pi tags

--- Core/AlarmProcessor/AlarmReader.cs
+++ Core/AlarmProcessor/AlarmReader.cs
@@ -23,6 +23,7 @@ namespace Core.AlarmProcessor
             _piCM = piCM;
         }
 
+        //Get Alarm String
         public void RetrieveAlarm()
         {
             string TagName = "CN.BJG.HMI0001.FLM01";
@@ -32,12 +33,15 @@ namespace Core.AlarmProcessor
             DateTime endTime = DateTime.Now;
             DateTime startTime = endTime.AddMinutes(-10);
 
+            //Create Start time and End Time
             AFTime startAFTime = new AFTime(startTime);
             AFTime endAFTime = new AFTime(endTime);
             AFTimeRange QueryRange = new AFTimeRange(startAFTime, endAFTime);
 
+            //Retrive PI data with time range
             AFValues valueList = AlarmPoint.RecordedValues(QueryRange, OSIsoft.AF.Data.AFBoundaryType.Inside, null, false);
 
+            //Filter the list of PI Data with "|ACTIVE|"
             var filteredActiveList = valueList.Where((item) =>
             {
                 return item.Value.ToString().Contains("|ACTIVE|");
@@ -47,28 +51,74 @@ namespace Core.AlarmProcessor
             {
                 _logger.Information("Timestamp : {0} Value : {1}", item.Timestamp, item.Value.ToString());
             }
-            
+
+            //Output source to a sourcelist from item
             var sourceList = filteredActiveList.Select((item) =>
             {
-                return item.Value.ToString().Split('|')[0];
+                // return as an AF Value
+                //return item.Value.ToString().Split('|')[0];
+                return new AFValue
+                {
+                    Timestamp = item.Timestamp,
+                    Value = item.Value.ToString().Split('|')[0]
+                };
             });
 
+            //Convert Source List to AFValues IList
+            AFValues AFsourceList = new AFValues();
+            foreach (var i in sourceList)
+            {
+                AFValue AFSourceItem = new AFValue(i.Value, i.Timestamp);
+                AFsourceList.Add(AFSourceItem);
+            }
+            //Find the SRC tag and update values into the tag
+            string SourceTagname = "CN.BJG.FLM01.ALYS.ALARMS.SRC.TEST";
+            PIPoint SourceTagPoint = PIPoint.FindPIPoint(_SitePI, SourceTagname);
+            var InsertSourceData = SourceTagPoint.UpdateValues(AFsourceList, OSIsoft.AF.Data.AFUpdateOption.Insert);
+        
+            //Output message to a messagelist from item
             var messageList = filteredActiveList.Select((item) =>
             {
-                return item.Value.ToString().Split('|')[3];
+                return new AFValue
+                {
+                    Timestamp = item.Timestamp,
+                    Value = item.Value.ToString().Split('|')[3]
+                };
             });
 
-            var numActive = filteredActiveList.ToList().Count;
+            //Convert Message List to AFValues IList
+            AFValues AFMessageList = new AFValues();
+            foreach (var i in messageList)
+            {
+                AFValue AFMessagItem = new AFValue(i.Value, i.Timestamp);
+                AFMessageList.Add(AFMessagItem);
+            }
+
+            //Find the MSG tag and update values into the tag
+            string MSGTagname = "CN.BJG.FLM01.ALYS.ALARMS.MSG.TEST";
+            PIPoint MSGTagPoint = PIPoint.FindPIPoint(_SitePI, MSGTagname);
+            var InsertMessageData = MSGTagPoint.UpdateValues(AFMessageList, OSIsoft.AF.Data.AFUpdateOption.Insert);
+
+            //Find the Count tag and update values into the tag
+            string CountTagname = "CN.BJG.FLM01.ALYS.ALARMS.COUNT.TEST";
+            PIPoint CountTagPoint = PIPoint.FindPIPoint(_SitePI, CountTagname);
+            var alarmCount = messageList.ToList().Count;
+            var timeNow = DateTime.Now;
+            AFValue numActive = new AFValue(alarmCount, timeNow);
+            CountTagPoint.UpdateValue(numActive, OSIsoft.AF.Data.AFUpdateOption.Insert);
+
+
+
 
             foreach (var item in messageList)
             {
-                _logger.Information("Message : {0}", item);
+                _logger.Information("Timstamp :{0}, Message : {1}", item.Timestamp , item.Value);
             }
             foreach (var item in sourceList)
             {
-                _logger.Information("Source : {0}", item);
+                _logger.Information("Timstamp :{0}, Message : {1}", item.Timestamp, item.Value);
             }
-            _logger.Information("Count : {0}", numActive);
+            _logger.Information("Timestamp :{0}, Count : {1}", timeNow, numActive);
 
             //_logger.Information("Number of ACTIVE Alarms {0}", filteredList);
 
